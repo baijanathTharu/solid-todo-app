8ace317a71321e0b7870f45dbd06bb7accbe1c51
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-05-18 21:58:52 +0545

    feat: todo read and write to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 
 import styles from './App.module.css';
 
+function readTodosFromLocalStorage() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
+
+function writeTodosToLocalStorage(todos: string[]) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 const App: Component = () => {
+  const [todo, setTodo] = createSignal('');
+  const [todos, setTodos] = createSignal(readTodosFromLocalStorage());
+
+  let todoInput: HTMLInputElement | undefined;
+
+  const addTodo = (todo: string) => {
+    setTodos([...todos(), todo]);
+    setTodo('');
+
+    writeTodosToLocalStorage(todos());
+
+    if (todoInput) {
+      todoInput.focus();
+    }
+  };
+
   return (
     <div class={styles.App}>
       <h2>my todo app</h2>
+      <form>
+        <input
+          ref={todoInput}
+          type='text'
+          name='todo'
+          // @ts-ignore
+          onInput={(e) => setTodo(e.target.value)}
+          value={todo()}
+        />
+        <button type='button' onClick={() => addTodo(todo())}>
+          Add
+        </button>
+      </form>
+
+      <ul>
+        {todos().map((todo: string) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 };
