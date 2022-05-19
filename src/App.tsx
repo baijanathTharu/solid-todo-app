0853b25ff67faae4cd84f013e7ce90f0c8b2aca2
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-05-19 20:41:08 +0545

    feat: add delete, undo, and compete button

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,36 @@ import { Component, createSignal } from 'solid-js';
 
 import styles from './App.module.css';
 
+interface ITodo {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+}
+
 function readTodosFromLocalStorage() {
   const todos = localStorage.getItem('todos');
   return todos ? JSON.parse(todos) : [];
 }
 
-function writeTodosToLocalStorage(todos: string[]) {
+function writeTodosToLocalStorage(todos: ITodo[]) {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 const App: Component = () => {
   const [todo, setTodo] = createSignal('');
-  const [todos, setTodos] = createSignal(readTodosFromLocalStorage());
+  const [todos, setTodos] = createSignal<ITodo[]>(readTodosFromLocalStorage());
 
   let todoInput: HTMLInputElement | undefined;
 
   const addTodo = (todo: string) => {
-    setTodos([...todos(), todo]);
+    setTodos([
+      ...todos(),
+      {
+        id: Math.ceil(Math.random() * 2000000),
+        text: todo,
+        isCompleted: false,
+      },
+    ]);
     setTodo('');
 
     writeTodosToLocalStorage(todos());
@@ -28,9 +41,39 @@ const App: Component = () => {
     }
   };
 
+  const completeTodo = (id: number) => {
+    setTodos(
+      todos().map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+    writeTodosToLocalStorage(todos());
+  };
+
+  const undoTodo = (id: number) => {
+    setTodos(
+      todos().map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: false } : todo
+      )
+    );
+    writeTodosToLocalStorage(todos());
+  };
+
+  const deleteTodo = (id: number) => {
+    if (window.confirm('Are you sure you want to delete this todo?')) {
+      setTodos(todos().filter((todo) => todo.id !== id));
+      writeTodosToLocalStorage(todos());
+    }
+  };
+
   return (
     <div class={styles.App}>
-      <h2>Solid Todo</h2>
+      <h2>Daily Todo Tracker</h2>
+
+      <h3>
+        Date: <span>{new Date().toLocaleDateString()}</span>
+      </h3>
+
       <form>
         <input
           ref={todoInput}
@@ -45,14 +88,41 @@ const App: Component = () => {
         </button>
       </form>
 
-      <ul class={styles.todoList}>
-        {todos().map((todo: string) => (
+      <ol class={styles.todoList}>
+        {todos().map((todo: ITodo, i) => (
           <li class={styles.todoItem}>
-            <span>{todo}</span>
-            <button>Delete</button>
+            <span class={todo.isCompleted ? styles.strikeThrough : ''}>
+              {' '}
+              {todo.text}
+            </span>
+            {!todo.isCompleted ? (
+              <button
+                class={styles.completeButton}
+                type='button'
+                onClick={() => completeTodo(todo.id)}
+              >
+                Done
+              </button>
+            ) : (
+              <button
+                class={styles.undoButton}
+                type='button'
+                onClick={() => undoTodo(todo.id)}
+              >
+                Undo
+              </button>
+            )}
+            {/* delete todo */}
+            <button
+              class={styles.deleteButton}
+              type='button'
+              onClick={() => deleteTodo(todo.id)}
+            >
+              Delete
+            </button>
           </li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 };
